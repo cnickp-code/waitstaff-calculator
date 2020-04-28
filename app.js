a71cb4e0d19111bd2f41d39a770eae8e8c27b948
Carlo Paredes <c.nick.paredes@gmail.com>
2020-04-28 14:32:18 -0700

    Added validate input, add item, and submit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function generatePage(store) {
     <div class="container">
         <div class="meal-details">
             <div class="meal-title">Enter the Meal Details</div>
-            <form class="meal-form">
+            <form id="meal-form">
                 <div class="meal-entry-container">
                     <div class="meal-text-container">
                         <div class="inner-text1">Base Meal Price:</div>
@@ -16,13 +16,13 @@ function generatePage(store) {
                     </div>
                     <div class="meal-input-container">
                         <div class="inner-text">&nbsp;$&nbsp; 
-                            <input type="text" name="base-price" class="js-input">
+                            <input type="text" name="base-price" class="js-input js-base-price">
                         </div>
                         <div class="inner-text">&nbsp;% 
-                            <input type="text" name="base-price" class="js-input">
+                            <input type="text" name="tax-rate" class="js-input js-tax-rate">
                         </div>
                         <div class="inner-text">&nbsp;% 
-                            <input type="text" name="base-price" class="js-input">
+                            <input type="text" name="tip-percentage" class="js-input js-tip-percentage">
                         </div>
                     </div>
                 </div>
@@ -43,10 +43,10 @@ function generatePage(store) {
                             <div class="charges-inner-text">Total:</div>
                         </div>
                         <div class="charges-right-container">
-                            <div class="charges-inner-text">&nbsp; 0.00</div>
-                            <div class="charges-inner-text">&nbsp; 0.00</div>  
+                            <div class="charges-inner-text meal-subtotal">&nbsp; 0.00</div>
+                            <div class="charges-inner-text meal-tip">&nbsp; 0.00</div>  
                             <div class="line"></div>
-                            <div class="charges-inner-text">&nbsp; 0.00</div>
+                            <div class="charges-inner-text meal-total">&nbsp; 0.00</div>
                         </div>
                     </div>
             </div>
@@ -59,9 +59,9 @@ function generatePage(store) {
                             <div class="charges-inner-text">Average Tip Per Meal:</div>
                         </div>
                         <div class="charges-right-container">
-                            <div class="charges-inner-text">&nbsp; 0.00 </div>
-                            <div class="charges-inner-text">&nbsp; 0</div>
-                            <div class="charges-inner-text">&nbsp; 0.00</div>
+                            <div class="charges-inner-text meal-tip-total">&nbsp; 0.00 </div>
+                            <div class="charges-inner-text meal-count">&nbsp; 0</div>
+                            <div class="charges-inner-text meal-average">&nbsp; 0.00</div>
                         </div>
                     </div>
             </div>
@@ -73,6 +73,47 @@ function generatePage(store) {
     </div>`;
 }
 
+// Math functions
+
+// Calculate subtotal (base meals + (base meals*tax rate))
+function calcSubtotal(meals) {
+
+}
+
+// Calculate tip for current meal
+function calcTips(meals) {
+
+}
+
+// Calculate total (add calcSubtotal and calcTips) 
+function calcTotal(meals) {
+
+}
+
+// Calculate tip total (add all tips)
+function calcTipTotal(meals) {
+
+}
+
+// Meal count (return store.length)
+function mealCount(meals) {
+
+}
+
+// Take calcTipTotal / mealCount
+function calcAverage(meals) {
+
+}
+
+// Reset page
+function handleReset() {
+    store = [
+        { basePrice: 0, taxRate: 0, tipPercent: 0 }
+    ];
+    renderPage();
+}
+
+// Render page
 function renderPage() {
     console.log(`Rendering page`);
 
@@ -81,8 +122,44 @@ function renderPage() {
     $('main').html(pageString);
 }
 
+// Validate input
+function isNumber(num) {
+    return /^\d+$/.test(num);
+}
+
+// Add item to store
+function addItemToStore(base, rate, percent) {
+    store.push({ basePrice: base, taxRate: rate, tipPercent: percent});
+}
+
+// Handle click submit
+function handleSubmitClicked() {
+    $('main').on('submit', '#meal-form', event => {
+        event.preventDefault();
+        console.log(`Submit button clicked. handleSubmitClicked ran`);
+        const basePrice = $('.js-base-price').val();
+        const taxRate = $('.js-tax-rate').val();
+        const tipPercentage = $('.js-tip-percentage').val();
+        console.log(basePrice);
+        console.log(taxRate);
+        console.log(tipPercentage);
+        
+        if(isNumber(basePrice) && isNumber(taxRate) && isNumber(tipPercentage)) {
+            console.log(`conditions are met`);
+            addItemToStore(basePrice, taxRate, tipPercentage);
+        } else {
+            alert('Must enter a valid number.');
+        }
+        
+        console.log(store);
+        renderPage();
+    });
+}
+
 function handleCalc() {
     renderPage();
+    handleSubmitClicked();
+    handleReset();
 }
 
 $(handleCalc);
